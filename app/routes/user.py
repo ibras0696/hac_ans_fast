40dcb28df479d7491c100a097f29c56d0d0411d7
DonviteCorleone <blackstar013.bs@gmail.com>
2025-07-09 21:09:28 +0300

    Сделал модификацию пользовательской страницы.

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,15 @@
-from fastapi import APIRouter
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import os
+
+router = FastAPI()
+
+TEMPLATES_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(TEMPLATES_DIR, "static"))
 
 
-router = APIRouter()
+@router.get("/")
+async def user(request: Request):
+    return templates.TemplateResponse(
+        "user.html",
+        {"request": request, "title": "Главная страница"})
